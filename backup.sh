a3a2938f4d71b27d368d7ab6a5ecc08494b0bc09
Joakim Melin <7058538+joakimmelin@users.noreply.github.com>
2025-10-24 13:59:09 +0200

    Update SCP command with new user and destination

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 NOW=$(date +%F)
 NAME=$(hostname)
 cp /usr/local/etc/config.xml /tmp/$NAME-config-$NOW.xml
-scp /tmp/$NAME-config-$NOW.xml root@10.0.2.11:gw01/
+scp /tmp/$NAME-config-$NOW.xml username@hostname:directory/
 rm /tmp/$NAME-config-$NOW.xml
 
